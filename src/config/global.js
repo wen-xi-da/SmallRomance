fdfe33f4d40e45ee1326e1a42f6ba5b930421336
文西 <49971400+wen-xi-da@users.noreply.github.com>
2024-11-28 20:58:28 +0800

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -59,16 +59,16 @@ const config = (function(){
     // type对应上面的color与font  若没有对应的 则默认为color1或font1
     dialogue:[
       {type:6, name:'朱', txt:'快过年了，我们去放烟花吧！'},
-      {type:2, name:'鄭', txt:'天还这么亮，现在放烟花也不好看。'},
-      {type:2, name:'鄭', txt:'再说你那有烟花吗？'},
+      {type:2, name:'涵', txt:'天还这么亮，现在放烟花也不好看。'},
+      {type:2, name:'涵', txt:'再说你那有烟花吗？'},
       {type:6, name:'朱', txt:'我当然有烟花啦。'},
       {type:6, name:'朱', txt:'你那有打火机吗？'},
-      {type:2, name:'鄭', txt:'没有呀～'},
+      {type:2, name:'涵', txt:'没有呀～'},
       {type:6, name:'朱', txt:'那你是怎么点燃我的心的？'},
-      {type:2, name:'鄭', txt:'。。。'},
-      {type:2, name:'鄭', txt:'可是我的心还没有被你点燃呀。'},
+      {type:2, name:'涵', txt:'。。。'},
+      {type:2, name:'涵', txt:'可是我的心还没有被你点燃呀。'},
       {type:6, name:'朱', txt:'别慌，我去买个打火机先。'},
-      {type:2, name:'鄭', txt:'打火机可点不燃我的心。'},
+      {type:2, name:'涵', txt:'打火机可点不燃我的心。'},
       {type:6, name:'朱', txt:'天快黑了，我要为你点燃整片天空。'},
     ],
 		// 阶段二
